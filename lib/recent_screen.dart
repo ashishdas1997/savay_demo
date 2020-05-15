bf1716ea795fb758f35dda0f2c2acbf06f6f016d
Ashish <ashishdas.1997@gmail.com>
2020-05-15 18:15:29 +0530

    Adding icon buttons

--- lib/recent_screen.dart
+++ lib/recent_screen.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class RecentScreen extends StatelessWidget {
   final String title;
   final String imageLink;
-  RecentScreen(this.title, this.imageLink);
+  final bool fav;
+  RecentScreen(this.title, this.imageLink, this.fav);
 
   @override
   Widget build(BuildContext context) {
@@ -15,70 +16,96 @@ class RecentScreen extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Card(
-          child: Wrap(
-            children: [
-              Column(
-                children: <Widget>[
-                  Image.asset(
-                    imageLink,
-                    width: 195,
-                    height: 115,
-                    fit: BoxFit.fitWidth,
-
-
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                          fontSize: 16, color: Color.fromRGBO(49, 67, 89, 0.8),
-                        fontWeight: FontWeight.w900
-
+            child: Wrap(
+              children: [
+                Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Image.asset(
+                        imageLink,
+                        width: 195,
+                        height: 115,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    Positioned(
+                      bottom: 5,
+                      right: 5,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 5, right: 5),
+                        margin: EdgeInsets.only(right: 5, bottom: 5),
+                        height: 25,
+                        width: 58,
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            if(fav) ? Icon(
+                            Icons.favorite,
+                          color: Colors.grey,
+                              size: 15,
+                          ),z
+                            VerticalDivider(),
+                            Icon(
+                              Icons.event_note,
+                              color: Colors.grey,
+                              size: 15,
+                            )
+                        ],),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(
-                          Icons.access_time,
-                          color: Colors.grey.shade400,
-                          size: 15 ,
-                        ),
-                        SizedBox(
-                          width: 3,
-                        ),
-                        Text(
-                          '13m ago',
-                          style: TextStyle(fontSize: 14, color: Colors.grey),
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Icon(
-                          Icons.audiotrack,
-                          size: 15,
-                          color: Colors.grey.shade400,
-                        ),
-                        SizedBox(
-                          height: 3,
-                        ),
-                        Text(
-                          'Audio',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        )
                       ],
                     ),
-                  )
-                ],
-              ),
-            ],
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromRGBO(49, 67, 89, 0.8),
+                            fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.access_time,
+                            color: Colors.grey.shade400,
+                            size: 15,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            '13m ago',
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Icon(
+                            Icons.audiotrack,
+                            size: 15,
+                            color: Colors.grey.shade400,
+                          ),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            'Audio',
+                            style: TextStyle(fontSize: 13, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ), ],
+
+                ),
+                ],)
+            ),
           ),
-        ),
-      ),
     );
   }
 }
